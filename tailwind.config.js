07d4e15d516cbf046aad0b1850e268d19969e1d3
asad-mytm <asad.raza@mytm.co>
2026-02-26 12:33:38 +0500

    Update Tailwind CSS configuration to simplify content paths by including all source files in a single pattern.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 
-  content: [
-    "./src/pages/InvestorPages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/ProductManagement/**/*.{js,ts,jsx,tsx}",
-    "./src/components/ui/**/*.{js,ts,jsx,tsx}",
-    "./src/components/language-switcher.tsx",
-    "./src/components/add-product-wizard.tsx",
-    "./src/components/product-category-wizard.tsx",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
